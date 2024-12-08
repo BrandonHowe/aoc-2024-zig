b936988e83978c73deed55d7cc2316e8d8e1880a
Brandon Howe <brandon.howe006@gmail.com>
2024-12-08 07:37:01 -0500

    day 8

--- src/day08.zig
+++ src/day08.zig
@@ -11,23 +11,114 @@ const gpa = util.gpa;
 const data = @embedFile("data/day08.txt");
 const stdout = std.io.getStdOut().writer();
 
-pub fn part1() !void
+const Antenna = struct
 {
-    try stdout.print("Part 1: {}\n", .{});
+    x: i16,
+    y: i16,
+    char: u8
+};
+
+pub fn part1(antennas: List(Antenna), width: i16, height: i16) !void
+{
+    var overlaps = Map(Antenna, bool).init(gpa);
+    defer overlaps.clearAndFree();
+    for (antennas.items) |baseAntenna|
+    {
+        for (antennas.items) |pairAntenna|
+        {
+            if (baseAntenna.char != pairAntenna.char) continue;
+            if (baseAntenna.x == pairAntenna.x and baseAntenna.y == pairAntenna.y) continue;
+            const dx = pairAntenna.x - baseAntenna.x;
+            const dy = pairAntenna.y - baseAntenna.y;
+            const a1 = Antenna{ .x = baseAntenna.x - dx, .y = baseAntenna.y - dy, .char = ' ' };
+            const a2 = Antenna{ .x = pairAntenna.x + dx, .y = pairAntenna.y + dy, .char = ' ' };
+            try overlaps.put(a1, true);
+            try overlaps.put(a2, true);
+        }
+    }
+    var total: i16 = 0;
+    var it = overlaps.keyIterator();
+    while (it.next()) |overlap|
+    {
+        if (overlap.x >= 0 and overlap.y >= 0 and overlap.x < width and overlap.y < height)
+        {
+            total += 1;
+        }
+    }
+    try stdout.print("Part 1: {}\n", .{total});
 }
 
-pub fn part2() !void
+pub fn part2(antennas: List(Antenna), width: i16, height: i16) !void
 {
-    try stdout.print("Part 2: {}\n", .{});
+    var overlaps = Map(Antenna, bool).init(gpa);
+    defer overlaps.clearAndFree();
+    for (antennas.items) |baseAntenna|
+    {
+        for (antennas.items) |pairAntenna|
+        {
+            if (baseAntenna.char != pairAntenna.char) continue;
+            if (baseAntenna.x == pairAntenna.x and baseAntenna.y == pairAntenna.y) continue;
+            const dx = pairAntenna.x - baseAntenna.x;
+            const dy = pairAntenna.y - baseAntenna.y;
+            var a1 = Antenna{ .x = baseAntenna.x, .y = baseAntenna.y, .char = ' ' };
+            try overlaps.put(a1, true);
+            while (a1.x >= 0 and a1.y >= 0 and a1.x < width and a1.y < height)
+            {
+                a1 = Antenna{ .x = a1.x - dx, .y = a1.y - dy, .char = ' ' };
+                try overlaps.put(a1, true);
+            }
+            var a2 = Antenna{ .x = pairAntenna.x, .y = pairAntenna.y, .char = ' ' };
+            try overlaps.put(a2, true);
+            while (a2.x >= 0 and a2.y >= 0 and a2.x < width and a2.y < height)
+            {
+                a2 = Antenna{ .x = a2.x + dx, .y = a2.y + dy, .char = ' ' };
+                try overlaps.put(a2, true);
+            }
+        }
+    }
+    var total: i16 = 0;
+    var it = overlaps.keyIterator();
+    while (it.next()) |overlap|
+    {
+        if (overlap.x >= 0 and overlap.y >= 0 and overlap.x < width and overlap.y < height)
+        {
+            total += 1;
+        }
+    }
+    try stdout.print("Part 2: {}\n", .{total});
 }
 
 pub fn main() !void
 {
+    const t0 = std.time.milliTimestamp();
+    var antennas: List(Antenna) = List(Antenna).init(gpa);
+    defer antennas.clearAndFree();
+    var x: i16 = 0;
+    var height: i16 = 0;
+    var width: i16 = 0;
+    for (data) |c|
+    {
+        if (c == '\n')
+        {
+            width = x;
+            height += 1;
+            x = 0;
+        }
+        else
+        {
+            if (c != '.')
+            {
+                try antennas.append(Antenna{ .x = x, .y = height, .char = c });
+            }
+            x += 1;
+        }
+    }
     const t1 = std.time.milliTimestamp();
-    try part1();
+    try stdout.print("Parsing: {}ms\n", .{t1 - t0});
+    try part1(antennas, width, height);
     const t2 = std.time.milliTimestamp();
     try stdout.print("Part 1: {}ms\n", .{t2 - t1});
-    try part2();
+    try part2(antennas, width, height);
     const t3 = std.time.milliTimestamp();
     try stdout.print("Part 2: {}ms\n", .{t3 - t2});
 }

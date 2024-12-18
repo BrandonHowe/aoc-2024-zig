884d76040f07394bb8e0f2f92edf527e95f43ae6
Brandon Howe <brandon.howe006@gmail.com>
2024-12-18 08:52:14 -0500

    day 17

--- src/day17.zig
+++ src/day17.zig
@@ -11,25 +11,119 @@ const gpa = util.gpa;
 const data = @embedFile("data/day17.txt");
 const stdout = std.io.getStdOut().writer();
 
-pub fn part1() !void
+const Program = struct
 {
-    try stdout.print("Part 1: {}\n", .{});
+    registerA: u64,
+    registerB: u64,
+    registerC: u64,
+    program: []u8,
+    instrPtr: usize
+};
+
+pub fn runProgram(registerA: u128, allocator: Allocator) ![]u8
+{
+    var outputs = List(u8).init(allocator);
+    defer outputs.clearAndFree();
+
+    var a = registerA;
+    while (a > 0)
+    {
+        var b = a % 8;
+        b = b ^ 3;
+        const c = a / @shlExact(@as(u64, 1), @as(u5, @intCast(b)));
+        b = b ^ 5;
+        a = a / 8;
+        b = b ^ c;
+        try outputs.append(@as(u8, @intCast(b % 8)));
+    }
+
+    return outputs.toOwnedSlice();
 }
 
-pub fn part2() !void
+pub fn part1(programBase: Program) !void
 {
-    try stdout.print("Part 2: {}\n", .{});
+    const output = try runProgram(programBase.registerA, gpa);
+    defer gpa.free(output);
+    try stdout.print("Part 1: {any}\n", .{output});
+}
+
+pub fn part2(programBase: Program) !void
+{
+    const buffer = try gpa.alloc(u8, 1000);
+    defer gpa.free(buffer);
+    var fba = std.heap.FixedBufferAllocator.init(buffer);
+    const allocator = fba.allocator();
+    // _ = allocator;
+    const program = programBase.program;
+
+    var matchedDigits: u8 = 0;
+    var base: u128 = 0;
+
+    while (matchedDigits < program.len)
+    {
+        var tempBase: u128 = 0;
+        for (1..100000000) |rawI|
+        {
+            const log8: u7 = @as(u7, @intCast(std.math.log2(rawI) / 3)) + 1;
+            const b1 = @shlExact(base, log8 * 3);
+            const i = b1 + rawI;
+            const res = try runProgram(i, allocator);
+            var matches: u8 = 0;
+            for (0..res.len) |j|
+            {
+                if (j >= program.len) break;
+                if (res[res.len - j - 1] == program[program.len - j - 1]) { matches += 1; }
+                else break;
+            }
+            if (matches > matchedDigits + 6 or matches >= program.len)
+            {
+                tempBase = i;
+                break;
+            }
+            fba.reset();
+        }
+        while (tempBase >= @shlExact(@as(u128, 1), @as(u7, @intCast((matchedDigits + 5) * 3))))
+        {
+            tempBase /= 8;
+        }
+        matchedDigits += 5;
+        base = tempBase;
+    }
+
+    try stdout.print("Part 2: {}\n", .{base});
 }
 
 pub fn main() !void
 {
+    var program = Program{ .registerA = 0, .registerB = 0, .registerC = 0, .program = std.mem.zeroes([]u8), .instrPtr = 0 };
+    var lines = splitSca(u8, data, '\n');
+    if (lines.next()) |regALine|
+    {
+        program.registerA = try parseInt(u64, regALine[12..], 10);
+    }
+    if (lines.next()) |regBLine|
+    {
+        program.registerB = try parseInt(u64, regBLine[12..], 10);
+    }
+    if (lines.next()) |regCLine|
+    {
+        program.registerC = try parseInt(u64, regCLine[12..], 10);
+    }
+    _ = lines.next();
+    if (lines.next()) |programLine|
+    {
+        const content = programLine[9..];
+        const nums = try util.splitScaToNum(u8, u8, content, ',', gpa);
+        program.program = nums;
+    }
     const t1 = std.time.milliTimestamp();
-    try part1();
+    try part1(program);
     const t2 = std.time.milliTimestamp();
     try stdout.print("Part 1: {}ms\n", .{t2 - t1});
-    try part2();
+    try part2(program);
     const t3 = std.time.milliTimestamp();
     try stdout.print("Part 2: {}ms\n", .{t3 - t2});
+    gpa.free(program.program);
 }
 
 // Useful stdlib functions

27ead11d48344517622efc7c2353c1d9becbf8e1
Brandon Howe <brandon.howe006@gmail.com>
2024-12-20 14:27:21 -0500

    day 20

--- src/day20.zig
+++ src/day20.zig
@@ -11,23 +11,229 @@ const gpa = util.gpa;
 const data = @embedFile("data/day20.txt");
 const stdout = std.io.getStdOut().writer();
 
-pub fn part1() !void
+const QueueItem = struct
 {
-    try stdout.print("Part 1: {}\n", .{});
+    x: usize,
+    y: usize
+};
+
+pub fn computeDistances() ![][]i16
+{
+    const grid = try util.splitInputIntoLines(data, gpa);
+    defer gpa.free(grid);
+    const width = grid[0].len;
+    var start_pos: usize = 0;
+    var end_pos: usize = 0;
+    for (grid, 0..) |line, y|
+    {
+        for (line, 0..) |char, x|
+        {
+            if (char == 'S') start_pos = y * width + x;
+            if (char == 'E') end_pos = y * width + x;
+        }
+    }
+
+    var visitedGrid = try gpa.alloc([]i16, grid.len);
+    for (visitedGrid) |*row|
+    {
+        row.* = try gpa.alloc(i16, width);
+
+        for (row.*) |*cell| {
+            cell.* = -1;
+        }
+    }
+
+    var visited = Map(QueueItem, bool).init(gpa);
+    defer visited.clearAndFree();
+    var queue = List(QueueItem).init(gpa);
+    defer queue.clearAndFree();
+    var next = List(QueueItem).init(gpa);
+    defer next.clearAndFree();
+    const startItem: QueueItem = QueueItem{
+        .x = start_pos % width,
+        .y = start_pos / width
+    };
+    try queue.append(startItem);
+    try visited.put(startItem, true);
+    visitedGrid[startItem.y][startItem.x] = 0;
+    var iter: i16 = 0;
+    mainLoop: while (queue.items.len > 0)
+    {
+        iter += 1;
+        for (queue.items) |node|
+        {
+            if (node.y == end_pos / width and node.x == end_pos % width)
+            {
+                break :mainLoop;
+            }
+        }
+        for (queue.items) |node|
+        {
+            if (node.y == end_pos / width and node.x == end_pos % width)
+            {
+                continue;
+            }
+            if (node.x > 0 and grid[node.y][node.x - 1] != '#')
+            {
+                const item = QueueItem{
+                    .x = node.x - 1,
+                    .y = node.y,
+                };
+                var canAdd = !visited.contains(item);
+                for (next.items) |i|
+                {
+                    if (std.meta.eql(i, item)) canAdd = false;
+                }
+                if (canAdd) try next.append(item);
+            }
+            if (node.x < width - 1 and grid[node.y][node.x + 1] != '#')
+            {
+                const item = QueueItem{
+                    .x = node.x + 1,
+                    .y = node.y,
+                };
+                var canAdd = !visited.contains(item);
+                for (next.items) |i|
+                {
+                    if (std.meta.eql(i, item)) canAdd = false;
+                }
+                if (canAdd) try next.append(item);
+            }
+            if (node.y > 0 and grid[node.y - 1][node.x] != '#')
+            {
+                const item = QueueItem{
+                    .x = node.x,
+                    .y = node.y - 1,
+                };
+                var canAdd = !visited.contains(item);
+                for (next.items) |i|
+                {
+                    if (std.meta.eql(i, item)) canAdd = false;
+                }
+                if (canAdd) try next.append(item);
+            }
+            if (node.y < grid.len - 1 and grid[node.y + 1][node.x] != '#')
+            {
+                const item = QueueItem{
+                    .x = node.x,
+                    .y = node.y + 1,
+                };
+                var canAdd = !visited.contains(item);
+                for (next.items) |i|
+                {
+                    if (std.meta.eql(i, item)) canAdd = false;
+                }
+                if (canAdd) try next.append(item);
+            }
+        }
+        queue.clearRetainingCapacity();
+        for (next.items) |item|
+        {
+            if (grid[item.y][item.x] == '#') continue;
+            try queue.append(item);
+            try visited.put(item, true);
+            visitedGrid[item.y][item.x] = iter;
+        }
+        next.clearRetainingCapacity();
+    }
+
+    return visitedGrid;
 }
 
-pub fn part2() !void
+pub fn part1(visitedGrid: [][]i16) !void
 {
-    try stdout.print("Part 2: {}\n", .{});
+    const width = visitedGrid[0].len;
+    var diffs = List(i16).init(gpa);
+    defer diffs.clearAndFree();
+    for (0..visitedGrid.len) |y|
+    {
+        for (0..width) |x|
+        {
+            const val = visitedGrid[y][x];
+            if (val == -1) continue;
+            if (x < width - 2)
+            {
+                const valR = visitedGrid[y][x + 2];
+                if (valR != -1)
+                {
+                    const diff = valR - val;
+                    const absDiff = if (diff < 0) -diff else diff;
+                    if (absDiff > 2) try diffs.append(absDiff - 2);
+                }
+            }
+            if (y < visitedGrid.len - 2)
+            {
+                const valD = visitedGrid[y + 2][x];
+                if (valD != -1)
+                {
+                    const diff = valD - val;
+                    const absDiff = if (diff < 0) -diff else diff;
+                    if (absDiff > 2) try diffs.append(absDiff - 2);
+                }
+            }
+        }
+    }
+
+    var total: u32 = 0;
+    for (diffs.items) |diff|
+    {
+        if (diff >= 100) total += 1;
+    }
+
+    try stdout.print("Part 1: {any}\n", .{total});
+}
+
+pub fn part2(visitedGrid: [][]i16) !void
+{
+    const width = visitedGrid[0].len;
+    var diffs = List(i32).init(gpa);
+    defer diffs.clearAndFree();
+    var y: i32 = 0;
+    const range = 20;
+    var total: u32 = 0;
+    while (y < visitedGrid.len) : (y += 1)
+    {
+        var x: i32 = 0;
+        while (x < width) : (x += 1)
+        {
+            const val = visitedGrid[@as(usize, @intCast(y))][@as(usize, @intCast(x))];
+            if (val == -1) continue;
+            
+            var dy: i32 = -range;
+            while (dy <= range) : (dy += 1)
+            {
+                if (y + dy < 0 or y + dy >= visitedGrid.len) continue;
+                const absYDiff: i32 = if (dy < 0) -dy else dy;
+                var dx: i32 = -range;
+                while (dx <= range) : (dx += 1)
+                {
+                    const absXDiff: i32 = if (dx < 0) -dx else dx;
+                    if (x + dx < 0 or x + dx >= width) continue;
+                    if (absXDiff + absYDiff > range) continue;
+
+                    const val2 = visitedGrid[@as(usize, @intCast(y + dy))][@as(usize, @intCast(x + dx))];
+                    if (val2 == -1) continue;
+                    const diff = val2 - val;
+                    const result = diff - (absYDiff + absXDiff);
+                    if (result >= 100) total += 1;
+                }
+            }
+        }
+    }
+
+    try stdout.print("Part 2: {any}\n", .{total});
 }
 
 pub fn main() !void
 {
+    const t0 = std.time.milliTimestamp();
+    const grid = try computeDistances();
     const t1 = std.time.milliTimestamp();
-    try part1();
+    try stdout.print("Computing distances: {}ms\n", .{t1 - t0});
+    try part1(grid);
     const t2 = std.time.milliTimestamp();
     try stdout.print("Part 1: {}ms\n", .{t2 - t1});
-    try part2();
+    try part2(grid);
     const t3 = std.time.milliTimestamp();
     try stdout.print("Part 2: {}ms\n", .{t3 - t2});
 }

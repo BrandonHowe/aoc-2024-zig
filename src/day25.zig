d4343491ee6fa94f63375fcf5220dc8f84275b62
Brandon Howe <brandon.howe006@gmail.com>
2024-12-25 06:33:30 -0500

    day 25

--- src/day25.zig
+++ src/day25.zig
@@ -11,14 +11,68 @@ const gpa = util.gpa;
 const data = @embedFile("data/day25.txt");
 const stdout = std.io.getStdOut().writer();
 
+const Pattern = struct
+{
+    isLock: bool,
+    pattern: [5]i8
+};
+
 pub fn part1() !void
 {
-    try stdout.print("Part 1: {}\n", .{});
+    const buffer = try gpa.alloc(u8, 10000);
+    defer gpa.free(buffer);
+    var fba = std.heap.FixedBufferAllocator.init(buffer);
+    const allocator = fba.allocator();
+
+    var locks = List(Pattern).init(gpa);
+    defer locks.clearAndFree();
+    var keys = List(Pattern).init(gpa);
+    defer keys.clearAndFree();
+
+    var it = splitSeq(u8, data, "\n\n");
+    while (it.next()) |str|
+    {
+        const lines = try util.splitInputIntoLines(str, allocator);
+        const isLock = lines[0][0] == '#';
+        var pattern = [5]i8{ 0, 0, 0, 0, 0 };
+        for (0..5) |x|
+        {
+            var fillCount: i8 = -1;
+            for (0..lines.len) |y|
+            {
+                if (lines[y][x] == '#') fillCount += 1;
+            }
+            pattern[x] = fillCount;
+        }
+        if (isLock)
+        {
+            try locks.append(Pattern{ .isLock = isLock, .pattern = pattern });
+        }
+        else
+        {
+            try keys.append(Pattern{ .isLock = isLock, .pattern = pattern });
+        }
+
+        fba.reset();
+    }
+    var successfulPairings: u32 = 0;
+    for (locks.items) |lock|
+    {
+        keyIt: for (keys.items) |key|
+        {
+            for (0..lock.pattern.len) |i|
+            {
+                if (lock.pattern[i] + key.pattern[i] > 5) continue :keyIt;
+            }
+            successfulPairings += 1;
+        }
+    }
+    try stdout.print("Part 1: {}\n", .{successfulPairings});
 }
 
 pub fn part2() !void
 {
-    try stdout.print("Part 2: {}\n", .{});
+    try stdout.print("Part 2: Merry Christmas!\n", .{});
 }
 
 pub fn main() !void
